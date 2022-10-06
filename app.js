0e876e4d79a1d0cb2227de9f1ba4a3a07da38860
Pedal <jmb181@humboldt.edu>
2022-10-05 21:56:48 -0700

    started node input

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-// console.log("Process", process);
-// console.log("process.argv", process.argv);
 
 const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
